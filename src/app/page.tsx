c5722ce1803e7845f43a73bdf2d6b74c6253c386
Hilary liu <2788370451@qq.com>
2024-12-21 23:12:32 +0800

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-blue-400 min-h-screen flex justify-center items-center text-3xl">
+        hello nextjs !!
+      </div>
+    </>
+  );
+}
